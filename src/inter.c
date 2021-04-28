d93e9783dc98710d92746c0a296816e0006db7e0
bygonexf <dream_star_xf@qq.com>
2021-04-28 10:35:53 +0800

    cr-8

--- src/inter.c
+++ src/inter.c
@@ -733,31 +733,31 @@ static int analyze_direct_skip(core_t *core, lbac_t *lbac_best)
 	*/
 	
 	if (cu_size_log2 == 6) {
-		satd_ratio_threshold = 0.959;
+		satd_ratio_threshold = 0.95;
 	}
 	else if (cu_size_log2 == 7) {
-		satd_ratio_threshold = 0.9675;
+		satd_ratio_threshold = 0.96;
 	}
 	else if (cu_size_log2 == 8) {
-		satd_ratio_threshold = 0.973;
+		satd_ratio_threshold = 0.968;
 	}
 	else if (cu_size_log2 == 9) {
-		satd_ratio_threshold = 0.9767;
+		satd_ratio_threshold = 0.9727;
 	}
 	else if (cu_size_log2 == 10) {
-		satd_ratio_threshold = 0.9788;
+		satd_ratio_threshold = 0.9755;
 	}
 	else if (cu_size_log2 == 11) {
-		satd_ratio_threshold = 0.9796;
+		satd_ratio_threshold = 0.9773;
 	}
 	else if (cu_size_log2 == 12) {
-		satd_ratio_threshold = 0.9807;
+		satd_ratio_threshold = 0.978;
 	}
 	else if (cu_size_log2 == 13) {
-		satd_ratio_threshold = 0.9815;
+		satd_ratio_threshold = 0.9785;
 	}
 	else if (cu_size_log2 == 14) {
-		satd_ratio_threshold = 0.9819;
+		satd_ratio_threshold = 0.979;
 	}
 	
 	
