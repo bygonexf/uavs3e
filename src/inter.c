c7869357c3d5ebd236b21e054d2cb73815c40c53
bygonexf <dream_star_xf@qq.com>
2021-05-03 10:37:30 +0800

    kkkkk

--- src/inter.c
+++ src/inter.c
@@ -886,7 +886,7 @@ static int analyze_direct_skip(core_t *core, lbac_t *lbac_best)
 		if (skip_idx < 1) {
 			int neb_same_mode_flag = 0;
 			for (int neb_idx = 0; neb_idx < 6; ++neb_idx) {
-				//if (valid_flag[neb_idx] && ((mode < 4 && (mode == neighbor_skip_mode[neb_idx])) || (cur_info->umve_flag && (cur_info->umve_idx % 4 == neb_umve_dir[neb_idx])))) {
+				/*
 				if (valid_flag[neb_idx] && (neb_umve_dir[neb_idx] >= 0)) {
 					printf("neb_idx:%d,umve_dir:%d\t", neb_idx, neb_umve_dir[neb_idx]);
 				}
@@ -901,6 +901,7 @@ static int analyze_direct_skip(core_t *core, lbac_t *lbac_best)
 					neb_same_mode_flag++;
 					printf("---hit_mode:%d\t", mode);
 				}
+				*/
 			}
 			if (neb_same_mode_flag){
 				//break;
@@ -925,13 +926,14 @@ static int analyze_direct_skip(core_t *core, lbac_t *lbac_best)
 	}
 	printf("\n");
 	*/
-
+	/*
 	if (mode_list[best_skip_idx] >= num_cands_woUMVE) {
 		printf("best_skip_idx:%d\tdir:%d\n", best_skip_idx, (mode_list[best_skip_idx]-num_cands_woUMVE)%4);
 	}
 	else {
 		printf("best_skip_idx:%d\tbest_mode:%d\n", best_skip_idx, mode_list[best_skip_idx]);
 	}
+	*/
 
 	return best_skip_idx;
 }
