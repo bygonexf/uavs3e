7bb2e79894ad9ac5481644a24cf3958a05911458
bygonexf <dream_star_xf@qq.com>
2021-05-05 09:55:27 +0800

    hisnocandi + earlyskip mode

--- src/uavs3e.c
+++ src/uavs3e.c
@@ -70,13 +70,15 @@ static core_t *core_alloc(com_info_t *info)
     core->sao_src_buf[2] = com_malloc(c_size);
     com_assert_rv(core->sao_src_buf[2], NULL);
 
-	core->inter_mode_map = com_malloc(3 * sizeof(u8) * info->f_scu);
+	core->inter_mode_map = com_malloc(1 * sizeof(u8) * info->f_scu);
 	core->inter_mode_map->map_skipidx = com_malloc(sizeof(u8) * info->f_scu);
 	com_assert_rv(core->inter_mode_map->map_skipidx, NULL);
+	/*
 	core->inter_mode_map->map_umveflag = com_malloc(sizeof(u8) * info->f_scu);
 	com_assert_rv(core->inter_mode_map->map_umveflag, NULL);
 	core->inter_mode_map->map_umveidx = com_malloc(sizeof(u8) * info->f_scu);
 	com_assert_rv(core->inter_mode_map->map_umveidx, NULL);
+	*/
 
     return core;
 }
@@ -96,8 +98,8 @@ static void core_free(core_t *core)
     com_mfree(core->sao_src_buf[2]);
 
 	com_mfree(core->inter_mode_map->map_skipidx);
-	com_mfree(core->inter_mode_map->map_umveflag);
-	com_mfree(core->inter_mode_map->map_umveidx);
+	//com_mfree(core->inter_mode_map->map_umveflag);
+	//com_mfree(core->inter_mode_map->map_umveidx);
 	com_mfree(core->inter_mode_map);
 
     com_mfree(core);
@@ -1163,13 +1165,14 @@ void *enc_pic_thread(enc_pic_t *ep, pic_thd_param_t *p)
 	for (int i = 0; i < info->f_scu; i++) {
 		core->inter_mode_map->map_skipidx[i] = -1;
 	}
-	
+	/*
 	for (int i = 0; i < info->f_scu; i++) {
 		core->inter_mode_map->map_umveflag[i] = -1;
 	}
 	for (int i = 0; i < info->f_scu; i++) {
 		core->inter_mode_map->map_umveidx[i] = -1;
 	}
+	*/
 	
 
 
